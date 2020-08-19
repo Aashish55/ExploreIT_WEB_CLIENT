29719e7acd007273bc946c23c833fcc0711f9170
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-08-19 21:39:07 +0545

    login/signup forms

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './style.css';
-import LoginImage from './../../images/adventure.jpg'
+// import LoginImage from './../../images/adventure.jpg'
 
 class Login extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <div className='Login__Section'>
-            <div className='Login__Container'>
-                <div className='imageSection'>
-                    <img className='loginImage' src={LoginImage} alt='LoginImage'/>
+    state = {}
+    render() {
+        return (
+            <div className='section'>
+                <div className='leftPart'>
+                    <h1>ExploreIT</h1>
+                    <p className='secondary_text'>Some Text Here...</p>
+                    <h2 className='arrowRight'>&rsaquo;</h2>
                 </div>
-                <div className="Login__Form">
-                    <input />
-                    <input />
-                </div>
-            </div>
+                <div className='form'>
+                    <h3>Login</h3>
+                    <h4>Email</h4>
+                    <input className='input' />
 
+                    <h4>Password</h4>
+                    <input className='input' />
+
+                    <button className='submit'>Login</button>
+
+                    <p className='switch_text'>Don't have an Account ? <a href='/registration'>Register Here.</a> </p>
+                </div>
             </div>
         );
     }
 }
- 
+
 export default Login;
--- src/Components/Auth/Registration.js
+++ src/Components/Auth/Registration.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class Registration extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <h2>This is Registration Screen</h2>
+    state = {}
+    render() {
+        return (
+            <div className='section'>
+                <div className='leftPart'>
+                    <h1>ExploreIT</h1>
+                    <p className='secondary_text'>Some Text Here...</p>
+                    <h2 className='arrowRight'>&rsaquo;</h2>
+                </div>
+                <div className='form'>
+                    <h3>Registration</h3>
+
+                    <h4>Full Name</h4>
+                    <input className='input' />
+
+                    <h4>Username</h4>
+                    <input className='input' />
+
+                    <h4>Gender</h4>
+                    <div className='gender'>
+                    <label className='genderLabel'>
+                        <input type="radio" value="Male" name='gender' className='radio' />
+                        Male
+                    </label>
+                    <label className='genderLabel'>
+                        <input type="radio" value="Female" name='gender' className='radio'/>
+                        Female
+                    </label>
+                    </div>
+
+                    <h4>Date of Birth</h4>
+                    <input className='input' disabled={true} value={Date.now().toString()} />
+
+                    <h4>Email</h4>
+                    <input className='input' />
+
+                    <h4>Phone Number</h4>
+                    <input className='input' type='number' />
+
+                    <h4>Country Code</h4>
+                    <input className='input' value='+977' />
+
+                    <h4>Password</h4>
+                    <input className='input' type='password' />
+
+                    <h4>Confirm Password</h4>
+                    <input className='input' type='password' />
+
+
+                    <button className='submit'>Sign Up</button>
+
+                    <p className='switch_text'>Already have an Account ? <a href='/login'>Login Here.</a> </p>
+                </div>
+            </div>
         );
     }
 }
- 
 export default Registration;
