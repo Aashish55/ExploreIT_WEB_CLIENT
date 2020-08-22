7109b98b79d8786c129b6d15789ca7b17ca3a1e0
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-08-22 15:19:52 +0545

    registration completed without styled components

--- src/Components/Auth/Registration.js
+++ src/Components/Auth/Registration.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './style.css'
-import { Link } from 'react-router-dom'
+import './style.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Registration extends Component {
     state = {
@@ -32,11 +33,30 @@ class Registration extends Component {
     handleSubmit = event => {
         event.preventDefault();
         if (this.isFormValid()) {
-            this.setState({ errors: [] });
             console.log('ready to post');
-            console.log(this.state)
+            this.setState({ errors: [] });
 
-
+            const registrationData = {
+                "name": this.state.fullName,
+                "username": this.state.userName,
+                "gender": this.state.gender.toUpperCase(),
+                "dob": this.state.dob,
+                "email": this.state.email,
+                "phoneNumber": this.state.phone,
+                "countryCode": this.state.countryCode,
+                "password": this.state.password,
+                "confirmPassword": this.state.passwordConfirmation,
+                "latitude": this.state.latitude.toString(),
+                "longitude": this.state.longitude.toString(),
+            }
+            console.log(registrationData)
+            axios.post('http://localhost:5000/api/v1/users', registrationData)
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     };
 
@@ -84,7 +104,12 @@ class Registration extends Component {
     displayErrors = errors => errors.map((error, i) => <p key={i} className='errorMessage'>{error.message}</p>);
 
     handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+        if (event.target.name === 'dob') {
+            this.setState({ dob: Date.now().toString() });
+        } else {
+            this.setState({ [event.target.name]: event.target.value });
+
+        }
     };
 
     render() {
@@ -109,52 +134,117 @@ class Registration extends Component {
                     <h2 className='arrowRight'> &rsaquo; </h2>
                 </div>
                 <form className='form' onSubmit={this.handleSubmit}>
+
                     <h3>Registration</h3>
 
                     <h4>Full Name</h4>
-                    <input name='fullName' onChange={this.handleChange} value={fullName} type='text' />
+                    <input
+                        name='fullName'
+                        onChange={this.handleChange}
+                        value={fullName}
+                        type='text'
+                    />
 
                     <h4>Username</h4>
-                    <input name='userName' onChange={this.handleChange} value={userName} type='text' />
+                    <input
+                        name='userName'
+                        onChange={this.handleChange}
+                        value={userName}
+                        type='text'
+                    />
 
                     <h4>Gender</h4>
                     <div className='gender'>
                         <label className='genderLabel'>
-                            <input type="radio" value="Male" name='gender' onChange={this.handleChange} className='radio' />
+                            <input type="radio"
+                                value="Male"
+                                name='gender'
+                                onChange={this.handleChange}
+                                className='radio'
+                            />
                         Male
                     </label>
                         <label className='genderLabel'>
-                            <input type="radio" value="Female" name='gender' onChange={this.handleChange} className='radio' />
+                            <input
+                                type="radio"
+                                value="Female"
+                                name='gender'
+                                onChange={this.handleChange}
+                                className='radio'
+                            />
                         Female
                     </label>
                     </div>
 
                     <h4>Date of Birth</h4>
-                    <input value={dob} name='dob' onChange={this.handleChange} type='text' />
+                    <input
+                        value={dob}
+                        name='dob'
+                        onChange={this.handleChange}
+                        type='text'
+                    />
 
                     <h4>Email</h4>
-                    <input name='email' onChange={this.handleChange} value={email} type='email' />
+                    <input
+                        name='email'
+                        onChange={this.handleChange}
+                        value={email}
+                        type='email'
+                    />
 
                     <h4>Phone Number</h4>
-                    <input type='number' name='phone' onChange={this.handleChange} value={phone} />
+                    <input
+                        type='number'
+                        name='phone'
+                        onChange={this.handleChange}
+                        value={phone}
+                    />
 
                     <h4>Country Code</h4>
-                    <input value={countryCode} onChange={this.handleChange} name='countryCode' type='text' />
+                    <input
+                        value={countryCode}
+                        onChange={this.handleChange}
+                        name='countryCode'
+                        type='text'
+                    />
 
                     <h4>Password</h4>
-                    <input type='password' name='password' onChange={this.handleChange} value={password} />
+                    <input
+                        type='password'
+                        name='password'
+                        onChange={this.handleChange}
+                        value={password}
+                    />
 
                     <h4>Confirm Password</h4>
-                    <input type='password' name='passwordConfirmation' onChange={this.handleChange} value={passwordConfirmation} />
+                    <input
+                        type='password'
+                        name='passwordConfirmation'
+                        onChange={this.handleChange}
+                        value={passwordConfirmation}
+                    />
 
                     {errors.length > 0 && (
                         <div className='errorSection'>Error:
                             {this.displayErrors(errors)}
                         </div>)}
 
-                    <button className='submit'>Sign Up</button>
+                    <button
+                        onClick={this.handleSubmit}
+                        className='submit'
+                    >
+                        Sign Up
+                    </button>
 
-                    <p className='switch_text'>Already have an Account ?  <Link to="/login">Login Here</Link></p>
+                    <p
+                        className='switch_text'
+                    >
+                        Already have an Account ?
+                    <Link to="/login"
+                        >
+                            Login Here
+                    </Link>
+                    </p>
                 </form>
             </div>
         );
