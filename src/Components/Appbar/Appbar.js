7b4a3f3b090985b080ba9434a00d73e2db9fd862
Aashish Jangam <aashish.jangam55@gmail.com>
2020-08-29 14:43:09 +0545

    appbar and layout completed

---
+++ src/Components/Appbar/Appbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../../images/Logo.png";
+import "./appbar.css";
+
+const Container = styled.div`
+  height: 8rem;
+  box-shadow: 0px 3px 2px rgba(0, 0, 0, 0.1);
+  padding: 1rem 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Appbar = () => (
+  <Container>
+    <img
+      src={logo}
+      style={{ height: "4rem", cursor: "pointer" }}
+      alt="ExploreIT"
+    />
+    <label>
+      <input type="checkbox" />
+      <span className="menu">
+        <div className="hamburger"></div>
+      </span>
+      <ul>
+        <li>
+          <a href="/option"> Option 1 </a>
+        </li>
+        <li>
+          <a href="/option"> Option 2 </a>
+        </li>
+        <li>
+          <a href="/option"> Option 3 </a>
+        </li>
+        <li>
+          <a href="/option"> Option 4 </a>
+        </li>
+      </ul>
+    </label>
+  </Container>
+);
+
+export default Appbar;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import AppBar from "./Appbar/Appbar";
 
+const Homepage = styled.div`
+  background-color: white;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const Main = styled.div`
+  width: 50%;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Description = styled.div`
+  width: 50%;
+  height: 100vh;
+  overflow-x: hidden;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background: linear-gradient(134.73deg, #c54409 0%, #f6763c 98.01%);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px 0px 0px 40px;
+`;
 
 class Home extends Component {
-    state={
-        user:this.props.currentUser
-    }
-    render() {
-        return (
-            <div>
-                This is home after login
-            </div>
-        )
-    }
-};
+  state = {
+    user: this.props.currentUser,
+  };
+  render() {
+    return (
+      <Homepage>
+        <Main>
+          <AppBar />
+        </Main>
+        <Description />
+      </Homepage>
+    );
+  }
+}
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProps)(Home);
