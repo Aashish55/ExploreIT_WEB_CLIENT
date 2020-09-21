441704bf08ef21c77d656a23124a8228388bcfc6
Aashish Jangam <aashish.jangam55@gmail.com>
2020-09-21 08:04:36 +0545

    states in nav links

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -28,12 +28,44 @@ const Info = styled.h3`
   color: white;
   padding-bottom: 1rem;
 `;
-const Features = styled.div``;
+const Features = styled.div`
+  margin:2rem 0;
+  width:90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Feature = styled.div`
+  width:100%;
+  background-color:#ffffff;
+  color:#c54409;
+  margin:1rem 0;
+  padding:.5rem;
+  font-size: 2.5rem;
+  text-align:center;
+  display:block;
+  border-top-right-radius:1rem;
+  border-bottom-left-radius:1rem;
+  cursor:pointer;
+  transition:all .2s linear;
+  &:hover{
+    background-color:#c54409;
+    color:#ffffff;
+  }
+  &:active{
+    width:80%;
+  }
+`;
 
 class Homepage extends Component {
   state = {
     user: this.props.currentUser,
     isLoading: false,
+    features:['Hotels','Adventures','Travels'],
+    hotelActiveState:true,
+    adventureActiveState:false,
+    travelActiveState:false,
   };
 
   logoutHandler = () => {
@@ -46,12 +78,26 @@ class Homepage extends Component {
    
   };
 
+  featureClickHandler = feature => {
+    if(feature==='Travels'){
+      this.setState({hotelActiveState:false,adventureActiveState:false, travelActiveState:true});
+    }else if(feature==='Adventures'){
+      this.setState({hotelActiveState:false,adventureActiveState:true, travelActiveState:false});
+    }else{
+      this.setState({hotelActiveState:true,adventureActiveState:false, travelActiveState:false});
+    }
+  }
+
   render() {
-    const { user, isLoading } = this.state;
+    const { user, isLoading, features } = this.state;
     return (
       <HomePageNavbar>
         <Info>{user.name.toUpperCase()}</Info>
-        <Features></Features>
+        <Features>
+          {features.map(feature=>(
+            <Feature key={feature} onClick={(feature)=>this.featureClickHandler} >{feature}</Feature>
+          ))}
+        </Features>
         <Button isLoading={isLoading} logoutHandler={this.logoutHandler} />
       </HomePageNavbar>
     );
